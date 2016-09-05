0c2e885432e9e2972f7168eefcd0a78e747f9296
EC2 Default User <ec2-user@www.macetw.com>
2016-09-05 13:49:06 +0000

    An httpd version of the same.

--- node/helloworld.js
+++ node/helloworld.js
@@ -1,1 +1,10 @@
-console.log("HelloWorld");
+var http = require('http');
+
+var server = http.createServer(function(request,response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("HelloWorld\n");
+})
+
+server.listen(8000);
+console.log("Server running atport 8000");
+
