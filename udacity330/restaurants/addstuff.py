6e07b5f1abf882426316b4ec22f9d77179a07d23
EC2 Default User <ec2-user@ip-172-31-55-246.ec2.internal>
2016-09-08 19:49:30 +0000

    Added first-lesson files.

---
+++ udacity330/restaurants/addstuff.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python26
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine( 'sqlite:///restaurantmenu.db' )
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
+myRestaurant = Restaurant(name = "Pizza Palace")
+session.add(myRestaurant)
+session.commit()
+
+cheesepizza = MenuItem( name="Cheese Pizza", description="this is a cheese pizza", course="dinner", price="$9.00", restaurant=myRestaurant)
+session.add(cheesepizza)
+session.commit()
+session.query(Restaurant).all()
+
+
+
