1163ba0c6905fb14faaadc6e827f5ca775129f43
Tyler Winter Mace <macetw@gmail.com>
2016-09-08 20:20:50 +0000

    Updated web server for handling a POST request.

--- udacity330/restaurants/website.py
+++ udacity330/restaurants/website.py
@@ -1,4 +1,7 @@
+#!/usr/bin/python26
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
 # handler ...
 class webserverHandler(BaseHTTPRequestHandler):
@@ -17,12 +20,39 @@ class webserverHandler(BaseHTTPRequestHandler):
       if self.path.endswith("hola"):
         output += "&#161Hola!"
 
+      output += "<form method='POST' enctype='multipart/form-data' action='hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+
       self.wfile.write("<html><body>%s <A HREF=%s>link</A></body><html>" % (output, defaultLink))
       print(output)
       return
 
     except:
       self.send_error(404, "File not found %s" % self.path)
+
+  def do_POST(self):
+    try:
+      self.send_response(301)
+      self.send_header('Content-type', 'text/html');
+      self.end_headers()
+
+      ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+      if ctype == 'multipart/form-data':
+        fields=cgi.parse_multipart(self.rfile,pdict)
+        messagecontent = fields.get('message')
+
+      output = ""
+      output += "<html><body>"
+      output += "<h2>Okay, how about this?</h2>"
+      output += "<h1> %s </h1>" % messagecontent[0]
+ 
+      output += "<form method='POST' enctype='multipart/form-data' action='hello'><h2>What would you like me to say?</h2><input name='message' type='text'><input type='submit' value='Submit'></form>"
+      output += "</html><body>"
+
+      self.wfile.write(output)
+      print output
+
+    except:
+      pass
 
 
 #main ...
