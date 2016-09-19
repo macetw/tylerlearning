8aec745104cd2078ad1c691bc1eae804cc3fc999
Tyler Winter Mace <macetw@gmail.com>
2016-09-19 04:22:03 +0000

    Implemented IaC for infrastructure requirments

---
+++ udacity330/adoptus/chef.pp
@@ -0,0 +1,17 @@
+service 'httpd' do
+  supports :restart => true, :reload => true
+  action :enable
+end
+
+package 'sqlite' do
+  action :install
+end
+
+package 'python27-pip' do
+  action :install
+end
+
+execute 'pip install flask'
+execute 'pip install sqlalchemy'
+
+
