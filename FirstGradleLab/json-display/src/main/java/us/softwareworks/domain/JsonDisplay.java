3c7f144ee2e3148eefe63c0943cf8e456573b620
Tyler Mace <macetw@gmail.com>
2019-11-10 20:04:50 -0500

    Working on Gradle lab from LinkedIn Learning.

---
+++ FirstGradleLab/json-display/src/main/java/us/softwareworks/domain/JsonDisplay.java
@@ -0,0 +1,15 @@
+package us.softwareworks.domain;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonDisplay {
+    public static String showJson(Object object) {
+        Gson gson = new GsonBuilder().create();
+        String json = gson.toJson(object);
+        System.out.println(json);
+
+        return json;
+
+    }
+}
