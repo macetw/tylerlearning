98c932ff183bf1e31bef3cfd52b9a90bd77e30f3
EC2 Default User <ec2-user@ip-172-31-72-230.ec2.internal>
2021-07-12 01:38:07 +0000

    Playing with a socket mode version?

---
+++ slackbot/socketmode/bot.py
@@ -0,0 +1,16 @@
+import os
+from slack_bolt import App
+
+
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"),
+          signing_secret=os.environ.get("SLACK_SIGNING_SECRET"))
+
+
+@app.message("hello")
+def message_hello(message, say):
+    say(f"Hey there <@{message['user']}>!")
+
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", "8080")))
+
+
